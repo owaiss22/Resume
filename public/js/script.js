3cde95d3abed5ca04c9c3cf3112f03d8883c9831
owaiss22 <owaisiqbalhussain@gmail.com>
2020-06-02 07:04:12 +0500

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,28 @@
+/* =========================================
+                Preloader
+============================================ */
+$(window).on('load', function () { // makes sure that whole site is loaded
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+});
+
+/* =========================================
+                Progress Bars
+============================================ */
+$(function () {
+
+    $("#progress-elements").waypoint(function () {
+
+        $(".progress-bar").each(function () {
+
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 2000);
+
+        });
+
+        this.destroy();
+    }, {
+        offset: 'bottom-in-view'
+    });
+});
